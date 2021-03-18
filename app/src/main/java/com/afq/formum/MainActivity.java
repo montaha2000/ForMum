c5b94d3178c5e2c65c5b8cc9580c91c1e30826c2
afnan <afnan.qdw@gmail.com>
2021-03-18 15:08:56 +0200

    Register + Login

--- app/src/main/java/com/afq/formum/MainActivity.java
+++ app/src/main/java/com/afq/formum/MainActivity.java
@@ -1,10 +1,19 @@
 package com.afq.formum;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import java.net.InetAddress;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +21,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        initView();
+    }
 
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
+    }
+
+    public boolean isInternetAvailable() {
+        try {
+            InetAddress ipAddr = InetAddress.getByName("google.com");
+            //You can replace it with your name
+            return !ipAddr.equals("");
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private void initView() {
+        rv = findViewById(R.id.rv);
     }
 }
